6ee714eefafa9faec3430bb757a1ecdd88b28fcf
Matt Madison <matt@madison.systems>
2015-06-06 07:23:38 -0700

    Update buildbot version

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='autobuilder',
-    version='0.6.5',
+    version='0.6.6',
     packages=find_packages(),
     license='MIT',
     author='Matt Madison',
-    author_email='madison@bliss-m.org',
-    install_requires=['buildbot>=0.8.10m2']
+    author_email='matt@madison.systems',
+    install_requires=['buildbot>=0.8.12m1']
 )
