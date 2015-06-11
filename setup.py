f31d7a224d0c5a3c2b1d2b16fb4bc74edefacf24
Matt Madison <matt@madison.systems>
2015-06-11 05:24:55 -0700

    Bump version string

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='autobuilder',
-    version='0.6.6',
+    version='0.6.7',
     packages=find_packages(),
     license='MIT',
     author='Matt Madison',
