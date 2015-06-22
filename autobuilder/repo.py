981911cc1f9988cb9506a69f48cab4ee178b8e90
Matt Madison <matt@madison.systems>
2015-06-22 15:34:49 -0700

    Remove the unused 'poll' setting for the  Repo class, and make 'pollinterval' default to None.

--- autobuilder/repo.py
+++ autobuilder/repo.py
@@ -4,11 +4,10 @@ Repository class.
 
 
 class Repo(object):
-    def __init__(self, name, uri, poll=True, pollinterval=300, project=None,
+    def __init__(self, name, uri, pollinterval=None, project=None,
                  submodules=False):
         self.name = name
         self.uri = uri
-        self.poll = poll
         self.pollinterval = pollinterval
         self.project = project or name
         self.submodules = submodules
