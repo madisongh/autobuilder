be18892774747007d8d1704bcc59d751fba92995
Matt Madison <matt@madison.systems>
2017-12-11 12:47:31 -0800

    Automatically retry file locks instead of failing.

--- autobuilder/utils/locks.py
+++ autobuilder/utils/locks.py
@@ -16,25 +16,25 @@ def lockfile(name, shared=False):
     if not os.path.exists(dirname):
         os.makedirs(dirname)
     f = None
-    # noinspection PyBroadException
-    try:
-        f = open(name, 'a+')
-        fno = f.fileno()
-        fcntl.flock(fno, fcntl.LOCK_SH if shared else fcntl.LOCK_EX)
-        stat1 = os.fstat(fno)
-        if os.path.exists(f.name):
-            stat2 = os.stat(f.name)
-            if stat1.st_ino == stat2.st_ino:
-                return f
-        f.close()
-    except Exception:
+    while True:
         # noinspection PyBroadException
         try:
+            f = open(name, 'a+')
+            fno = f.fileno()
+            fcntl.flock(fno, fcntl.LOCK_SH if shared else fcntl.LOCK_EX)
+            stat1 = os.fstat(fno)
+            if os.path.exists(f.name):
+                stat2 = os.stat(f.name)
+                if stat1.st_ino == stat2.st_ino:
+                    return f
             f.close()
         except Exception:
+            # noinspection PyBroadException
+            try:
+                f.close()
+            except Exception:
+                pass
             pass
-        pass
-    return None
 
 
 def unlockfile(f):
