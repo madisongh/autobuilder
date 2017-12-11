5ee849a6473be8530f9336ca96d9be8495a34c16
Matt Madison <matt@madison.systems>
2017-12-11 07:37:49 -0800

    Import scripts used on workers, and add a new one for updating a downloads mirror.

---
+++ autobuilder/scripts/install_sdk.py
@@ -0,0 +1,264 @@
+#!/usr/bin/env python
+# ex:ts=4:sw=4:sts=4:et
+# -*- tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*-
+# Utility script for installing an SDK from tmp/deploy/sdk
+
+import os
+import sys
+import re
+import optparse
+import time
+
+from autobuilder.utils.logutils import Log
+from autobuilder.utils import process
+
+__version__ = "0.2.1"
+
+log = Log(__name__)
+
+
+def get_info(histdir, sdkname):
+    infodict = {}
+    pat = re.compile('^(.+?)\s*=\s*(.+)\n')
+    infodir = os.path.join(histdir, sdkname)
+    infofilename = os.path.join(infodir, 'sdk-info.txt')
+    if not os.path.exists(infofilename):
+        subdirs = os.listdir(infodir)
+        if len(subdirs) != 1:
+            raise RuntimeError("unable to identify SDK info for %s" % sdkname)
+        infofilename = os.path.join(infodir, subdirs[0], 'sdk-info.txt')
+    log.debug("SDK info file name for %s: %s", sdkname, infofilename)
+    infofile = open(infofilename, 'r')
+    for line in infofile:
+        m = pat.match(line)
+        if m is None:
+            log.warn('Unparseable line in SDK %s info file:\n  %s',
+                     sdkname, line)
+            continue
+        infodict[m.group(1)] = m.group(2)
+        log.debug('%s = %s', m.group(1), m.group(2))
+    infofile.close()
+    return infodict
+
+
+def find_default_install_dir(installerfile):
+    pat = re.compile(r'^DEFAULT_INSTALL_DIR="(.+)"$')
+    f = open(installerfile, 'r')
+    for line in f:
+        m = pat.match(line)
+        if m is not None:
+            f.close()
+            return m.group(1)
+    f.close()
+    return None
+
+
+class Sdk:
+    def __init__(self, name, histdir):
+        self.name = name
+        self.infodict = get_info(histdir, name)
+
+    def installerfile(self):
+        return self.infodict['SDK_NAME'] + '-toolchain-' + self.infodict['SDK_VERSION'] + '.sh'
+
+    def sdk_name(self):
+        return self.infodict['SDK_NAME']
+
+    def sdk_version(self):
+        return self.infodict['SDK_VERSION']
+
+    def distro(self):
+        return self.infodict['DISTRO']
+
+    def distro_version(self):
+        return self.infodict['DISTRO_VERSION']
+
+    def sdksize(self):
+        return int(self.infodict['SDKSIZE'])
+
+    def sdkmachine(self):
+        return self.infodict['SDKMACHINE']
+
+
+def main():
+    global log
+    parser = optparse.OptionParser(
+        version="%prog version " + __version__,
+        usage="""%prog [options] [sdk-name...]
+
+Installs one or more SDKs created by a bitbake build, with
+some validation checks to ensure that the new SDK does not
+overwrite one that is already installed.
+
+If you do not specify an SDK to install, all available SDKs
+will be installed by default.  Use the --list option to list
+the SDKs that the script can locate.
+
+""")
+
+    parser.add_option('-D', '--deploy-dir',
+                      help='location of SDK deploy directory (default is tmp/deploy/sdk)',
+                      action='store', dest='deploy_dir', default='tmp/deploy/sdk')
+    parser.add_option('-H', '--history-dir',
+                      help='location of buildhistory directory (default is buildhistory)',
+                      action='store', dest='history_dir', default='buildhistory'),
+    parser.add_option('-d', '--debug', help='increase the debug level',
+                      action='count', dest='debug', default=0)
+    parser.add_option('-v', '--verbose', help='verbose output',
+                      action='store_true', dest='verbose')
+    parser.add_option('-n', '--dry-run',
+                      help='show, but do not execute, the generated commands',
+                      action='store_true', dest='dry_run')
+    parser.add_option('-i', '--install-root', help='Installation directory root',
+                      action='store', dest='install_root')
+    parser.add_option('-s', '--date-stamp', help='Date stamp to append to version string',
+                      action='store', dest='date_stamp')
+    parser.add_option('', '--no-stamp', help='Do not append date stamp to version string',
+                      action='store_true', dest='nostamp')
+    parser.add_option('-u', '--update-current', help='update the "current" symlink',
+                      action='store_true', dest='update_current')
+    parser.add_option('-l', '--list', help='List available SDKs instead of installing',
+                      action='store_true', dest='do_list')
+    parser.add_option('-m', '--machine', help='Target MACHINE name',
+                      action='store', dest='machine')
+    parser.add_option('', '--image', help='Image name', action='store', dest='image')
+
+    options, args = parser.parse_args()
+
+    log.set_level(options.debug, options.verbose)
+
+    hdir = os.path.realpath(os.path.join(options.history_dir, 'sdk'))
+    if not os.path.isdir(hdir):
+        log.error("not a directory: %s (buildhistory)", hdir)
+        return 1
+
+    ddir = os.path.realpath(options.deploy_dir)
+    if not os.path.isdir(ddir):
+        log.error("not a directory: %s (deploy dir)", ddir)
+        return 1
+
+    sdkset = set(os.listdir(hdir))
+    if len(sdkset) == 0:
+        log.error("no SDKs found in %s", hdir)
+        return 1
+
+    # Make it easier on the autobuilder by allowing it to pass in
+    # null arguments that we don't bother to interpret
+    args = [arg for arg in args if arg != '']
+    if len(args) > 0 and not options.do_list:
+        argset = set(args)
+        if not argset.issubset(sdkset):
+            log.error("Unknown SDKs requested: [%s]",
+                      ','.join(list(argset - sdkset)))
+            return 1
+        sdkset = argset
+
+    sdklist = [Sdk(name, hdir) for name in sorted(list(sdkset))]
+
+    if options.do_list:
+        log.plain("Available SDKs:\n    %s\n",
+                  '\n    '.join([sdk.name for sdk in sdklist]))
+        return 0
+
+    if not options.machine:
+        log.error("missing --machine specifier")
+        return 1
+
+    if not options.image:
+        log.error("missing --image specifier")
+        return 1
+
+    if options.image == 'buildtools-tarball':
+        options.image = 'buildtools'
+        options.machine = ''
+
+    if not options.date_stamp:
+        options.date_stamp = time.strftime("%Y%m%d")
+
+    error_count = 0
+
+    for sdk in sdklist:
+        target = options.machine.replace('_', '-')
+        installer = os.path.join(ddir, sdk.installerfile())
+        if options.install_root:
+            if options.nostamp:
+                lastdir = sdk.sdk_version()
+            else:
+                lastdir = sdk.sdk_version().replace('-snapshot', '') + '-' + options.date_stamp
+            destdir = os.path.join(options.install_root, target, lastdir)
+            if os.path.exists(destdir):
+                if options.nostamp:
+                    log.error("destination directory %s exists - skipping", destdir)
+                    error_count += 1
+                    break
+                for tagnum in range(99):
+                    tag = "-%02d" % (tagnum + 1)
+                    if not os.path.exists(destdir + tag):
+                        destdir += tag
+                        break
+                else:
+                    log.error("destination directory %s exists - skipping",
+                              destdir)
+                    error_count += 1
+                    continue
+                log.verbose("Destination: %s", destdir)
+        else:
+            destdir = None
+            default_dest = find_default_install_dir(installer)
+            log.debug('for %s, default installation directory: %s',
+                      sdk.name, default_dest)
+            if default_dest is not None and os.path.exists(default_dest):
+                log.error("attempting to install over an existing SDK at %s - skipping",
+                          default_dest)
+                error_count += 1
+                continue
+
+        if options.dry_run:
+            log.plain("bash %s%s -y", installer,
+                      (" -d %s" % destdir) if destdir else "")
+        else:
+            # noinspection PyBroadException
+            try:
+                if destdir is not None:
+                    cmd = "%s -d %s -y" % (installer, destdir)
+                else:
+                    cmd = "%s -y" % installer
+                log.note("executing %s", cmd)
+                (output, errors) = process.run(cmd)
+                log.plain("%s", output)
+            except Exception:
+                log.error("error installing %s:\n%s", sdk.name, errors)
+                error_count += 1
+                continue
+
+        if destdir is not None:
+            (parent, dest) = os.path.split(destdir)
+            if options.update_current:
+                cmd = "ln -snf %s %s" % (dest, os.path.join(parent, 'current'))
+                if options.dry_run:
+                    log.plain(cmd)
+                    continue
+                else:
+                    # noinspection PyBroadException
+                    try:
+                        log.note("executing %s", cmd)
+                        (output, errors) = process.run(cmd)
+                        log.plain("%s", output)
+                    except Exception:
+                        log.warn("error updating current symlink:\n%s", errors)
+
+    return error_count
+
+
+if __name__ == "__main__":
+    # noinspection PyBroadException
+    try:
+        ret = main()
+        sys.exit(ret)
+    except SystemExit:
+        pass
+    except Exception:
+        import traceback
+
+        traceback.print_exc(5)
+        sys.exit(1)
---
+++ autobuilder/scripts/update_sstate_mirror.py
@@ -0,0 +1,213 @@
+#!/usr/bin/env python
+# Copyright 2014 by Matthew Madison
+# Distributed under license.
+
+import os
+import sys
+import re
+import stat
+import optparse
+import shutil
+from datetime import date, timedelta
+from autobuilder.utils.logutils import Log
+
+__version__ = '0.2.2'
+
+log = Log(__name__)
+
+
+def do_cleanup(mirrorbase, subdir, options):
+    """
+    Walks the sstate-mirror tree, pruning any shared state
+    files that are old enough (i.e., have a modification time
+    older than prune_age days).
+
+    Returns the number of files removed.
+    """
+    prune_age = timedelta(options.prune_age)
+    now = date.today()
+    removal_count = 0
+    for dirpath, _, filenames in os.walk(os.path.join(mirrorbase, subdir)):
+        for filename in filenames:
+            if not (filename.endswith('.tgz') or filename.endswith('.siginfo')):
+                log.debug(2, 'Not cleaning up %s', filename)
+                continue
+            mirrorfile = os.path.join(dirpath, filename)
+            if os.path.islink(mirrorfile):
+                log.warn('Found symlink in mirror: %s', mirrorfile)
+                if not options.dry_run:
+                    log.verbose('Removing symlink from mirror: %s', mirrorfile)
+                    os.unlink(mirrorfile)
+                continue
+            statinfo = os.stat(mirrorfile)
+            mtime = date.fromtimestamp(statinfo[stat.ST_MTIME])
+            if now < mtime:
+                log.warn('Modification time for %s (%s) ' +
+                         'is later than today (%s)',
+                         os.path.join(dirpath, filename), mtime.isoformat(),
+                         now.isoformat())
+                continue
+            if now - mtime > prune_age:
+                log.debug(1, '%s is old (mtime %s)', mirrorfile,
+                          mtime.isoformat())
+                removal_count += 1
+                if options.dry_run:
+                    log.plain('rm -f %s', mirrorfile)
+                else:
+                    log.verbose('Removing: %s', mirrorfile)
+                    os.unlink(mirrorfile)
+    return removal_count
+
+
+def do_copy(cachebase, subdir, mirrorbase, options):
+    """
+    Walks the local sstate-cache tree, copying any shared-state packages
+    created up to the corresponding location in the sstate-mirror tree,
+    and updating the modification time for any packages in the sstate-mirror
+    that are symlinked in the local cache (so we know they were just used).
+
+    Returns the number of files copied.
+    """
+    copy_count = 0
+    for dirpath, _, filenames in os.walk(os.path.join(cachebase, subdir)):
+        for filename in filenames:
+            if not (filename.endswith('.tgz') or filename.endswith('.siginfo')):
+                log.debug(2, 'Skipping copy of %s', filename)
+                continue
+            cachefile = os.path.join(dirpath, filename)
+            if os.path.islink(cachefile):
+                mirrorfile = os.path.realpath(os.readlink(cachefile))
+                log.verbose('Updating modification time of %s', mirrorfile)
+                if options.dry_run:
+                    log.plain('touch %s', mirrorfile)
+                else:
+                    # noinspection PyBroadException
+                    try:
+                        os.utime(mirrorfile, None)
+                    except Exception:
+                        log.warn('Error occurred trying to update %s',
+                                 mirrorfile)
+                        pass
+                continue
+            relpath = os.path.relpath(cachefile, cachebase)
+            mirrorfile = os.path.join(mirrorbase, relpath)
+            mirrordir = os.path.dirname(mirrorfile)
+            copy_count += 1
+            if options.dry_run:
+                log.plain('test -d %s || mkdir -p %s', mirrordir, mirrordir)
+                log.plain('cp %s %s', cachefile, mirrordir)
+            else:
+                log.verbose('Copying %s to %s', cachefile, mirrordir)
+                if not os.path.isdir(mirrordir):
+                    os.makedirs(mirrordir)
+                try:
+                    shutil.copy(cachefile, mirrorfile)
+                except IOError as err:
+                    log.warn('Error occurred (errno=%d) copying %s to %s',
+                             err.errno, cachefile, mirrorfile)
+    return copy_count
+
+
+def main():
+    global log
+    parser = optparse.OptionParser(
+        version="%prog version " + __version__,
+        usage="""%prog [options] dirname
+
+Updates a shared-state mirror after a build.  Two modes of operation:
+  Update mode:
+    * copies sstate-* packages that were created during the build to
+      the mirror location
+    * touches sstate-* packages in the mirror location that were used
+      during the build (to update the modification time)
+  Clean mode:
+    * removes sstate-* packages from the mirror directory that exceed
+      a specified age and were not used in the build
+
+Run this tool in update mode after each build, or each sub-build in a
+set of related builds comprising a single build run.  Once a build run
+has been completed, run this tool in clean mode to prune out old sstate
+packages.
+""")
+
+    parser.add_option('-m', '--mode',
+                      help='operation mode: update (default) or clean',
+                      action='store', dest='mode', default='update',
+                      type='choice', choices=['update', 'clean'])
+    parser.add_option('-s', '--sstate-dir',
+                      help='location of sstate-cache directory from build',
+                      action='store', dest='sstate_dir', default='sstate-cache')
+    parser.add_option('-d', '--debug', help='increase the debug level',
+                      action='count', dest='debug', default=0)
+    parser.add_option('-v', '--verbose', help='verbose output',
+                      action='store_true', dest='verbose')
+    parser.add_option('-n', '--dry-run',
+                      help='display commands instead of executing them',
+                      action='store_true', dest='dry_run')
+    parser.add_option('-a', '--prune-age',
+                      help='age, in days, to qualify files for removal',
+                      action='store', dest='prune_age', type='int', default=30)
+    options, args = parser.parse_args()
+    if len(args) < 1:
+        raise RuntimeError('no sstate-mirror directory name specified')
+    if not os.path.isdir(args[0]):
+        if not os.path.exists(args[0]):
+            os.makedirs(args[0])
+        else:
+            raise RuntimeError('sstate-mirror directory %s not found' % args[0])
+    log.set_level(options.debug, options.verbose)
+    mirrorbase = os.path.realpath(args[0])
+    if options.mode == 'clean':
+        rmcount = 0
+        for subdir in os.listdir(mirrorbase):
+            rmcount += do_cleanup(mirrorbase, subdir, options)
+        if options.dry_run:
+            log.plain('# CLEAN: %d removals', rmcount)
+        else:
+            log.note('Removed %d stale entries', rmcount)
+    elif options.mode == 'update':
+        if not os.path.isdir(options.sstate_dir):
+            log.note('sstate-cache directory %s not found - nothing to do',
+                     options.sstate_dir)
+            return 0
+        lsbstr = None
+        twohex = re.compile(r'^[0-9a-f][0-9a-f]$')
+        lsbpat = re.compile(r'^(universal|[a-zA-z]+-[0-9]+\.[0-9]+)$')
+        for subdir in os.listdir(options.sstate_dir):
+            if lsbpat.match(subdir):
+                log.debug(1, 'Found LSB subdirectory: %s', subdir)
+                if lsbstr is not None:
+                    log.warn('Multiple LSB subdirectories found')
+                else:
+                    lsbstr = subdir
+                continue
+            if not twohex.match(subdir):
+                log.warn('Unrecognized directory found in sstate-cache: %s',
+                         subdir)
+        cachebase = os.path.realpath(options.sstate_dir)
+        cpcount = 0
+        for subdir in os.listdir(cachebase):
+            if subdir != lsbstr and not twohex.match(subdir):
+                log.debug(1, 'Skipping copy of %s',
+                          os.path.join(cachebase, subdir))
+            else:
+                cpcount += do_copy(cachebase, subdir, mirrorbase, options)
+        if options.dry_run:
+            log.plain('# UPDATE: %d copies', cpcount)
+        else:
+            log.note('Copied %d new entries', cpcount)
+    return 0
+
+
+if __name__ == "__main__":
+    # noinspection PyBroadException
+    try:
+        ret = main()
+        sys.exit(ret)
+    except SystemExit:
+        pass
+    except Exception:
+        import traceback
+
+        traceback.print_exc(5)
+        sys.exit(1)
