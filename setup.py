a34ba94f44ebbda70e2a4b2d9ec34017d1683f0f
Matt Madison <matt@madison.systems>
2017-12-11 12:26:59 -0800

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='autobuilder',
-    version='0.11.4',
+    version='0.11.5',
     packages=find_packages(),
     license='MIT',
     author='Matt Madison',
