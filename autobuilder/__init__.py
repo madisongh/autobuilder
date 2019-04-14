123fe3a9e4a6df0590232fa441979190a63ae719
Matt Madison <matt@madison.systems>
2019-04-14 09:07:27 -0700

    Export TargetImage and SdkImage

--- autobuilder/__init__.py
+++ autobuilder/__init__.py
@@ -1,4 +1,5 @@
 from autobuilder.abconfig import AutobuilderConfig, Buildtype, Distro, Repo, TargetImageSet
+from autobuilder.abconfig import TargetImage, SdkImage
 from autobuilder.abconfig import AutobuilderWorker, EC2Params, AutobuilderEC2Worker
 from autobuilder.abconfig import AutobuilderGithubEventHandler
 from autobuilder.ec2 import MyEC2LatentWorker
