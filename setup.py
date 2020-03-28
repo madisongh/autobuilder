cf3bf0d8f59c56d033285d2d2add8fa05070c3c6
Matt Madison <matt@madison.systems>
2020-03-28 07:11:46 -0700

    Bump version to 2.5.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ BUILDBOTVERSION = '2.7.0'
 
 setup(
     name='autobuilder',
-    version='2.5.0',
+    version='2.5.1',
     packages=find_packages(),
     license='MIT',
     author='Matt Madison',
