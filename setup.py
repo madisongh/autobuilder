99dc9e4c389b54778da4e3c1127759728a770806
Matt Madison <matt@madison.systems>
2020-04-14 12:05:21 -0700

    Move store_artifacts script to a separate repository.

--- setup.py
+++ setup.py
@@ -4,23 +4,17 @@ BUILDBOTVERSION = '2.7.0'
 
 setup(
     name='autobuilder',
-    version='2.5.1',
+    version='2.5.99',
     packages=find_packages(),
     license='MIT',
     author='Matt Madison',
     author_email='matt@madison.systems',
-    entry_points={
-        'console_scripts': [
-            'store-artifacts = autobuilder.scripts.store_artifacts:main',
-        ]
-    },
     include_package_data=True,
     package_data={
         'autobuilder': ['templates/*.txt']
     },
     install_requires=['aws-secretsmanager-caching',
                       'buildbot[tls]>=' + BUILDBOTVERSION,
-                      'buildbot-worker>=' + BUILDBOTVERSION,
                       'buildbot-www>=' + BUILDBOTVERSION,
                       'buildbot-console-view>=' + BUILDBOTVERSION,
                       'buildbot-grid-view>=' + BUILDBOTVERSION,
