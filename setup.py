9c357eb99036d0b9923ea9431851c8bf19abd0af
Matt Madison <matt@madison.systems>
2020-09-28 03:39:18 -0700

    Bump version to 2.7.8.
    Signed-off-by: Matt Madison <matt@madison.systems>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ BUILDBOTVERSION = '2.8.4'
 
 setup(
     name='autobuilder',
-    version='2.7.7',
+    version='2.7.8',
     packages=find_packages(),
     license='MIT',
     author='Matt Madison',
