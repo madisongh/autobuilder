afda48f35814a05b8ab0121ff3ec09087f42fc29
Matt Madison <matt@madison.systems>
2021-01-12 04:25:58 -0800

    Bump version to 3.1.0
    Signed-off-by: Matt Madison <matt@madison.systems>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ BUILDBOTVERSION = '2.10.0'
 
 setup(
     name='autobuilder',
-    version='3.0.0',
+    version='3.1.0',
     packages=find_packages(),
     license='MIT',
     author='Matt Madison',
