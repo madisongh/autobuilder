c28f156c2ee018ae3966bb066cbddf95efd9b733
Matt Madison <matt@madison.systems>
2022-01-24 04:48:04 -0800

    Convert to using setup.cfg and pyproject.toml
    Signed-off-by: Matt Madison <matt@madison.systems>

--- setup.py
+++ setup.py
@@ -1,27 +1,2 @@
-from setuptools import setup, find_packages
-
-BUILDBOTVERSION = '3.4.0'
-
-setup(
-    name='autobuilder',
-    version='3.7.6',
-    packages=find_packages(),
-    license='MIT',
-    author='Matt Madison',
-    author_email='matt@madison.systems',
-    include_package_data=True,
-    package_data={
-        'autobuilder': ['templates/*.txt']
-    },
-    install_requires=['aws-secretsmanager-caching',
-                      'buildbot[tls]>=' + BUILDBOTVERSION,
-                      'buildbot-www>=' + BUILDBOTVERSION,
-                      'buildbot-console-view>=' + BUILDBOTVERSION,
-                      'buildbot-grid-view>=' + BUILDBOTVERSION,
-                      'buildbot-waterfall-view>=' + BUILDBOTVERSION,
-                      'buildbot-badges>=' + BUILDBOTVERSION,
-                      'boto3', 'botocore',
-                      'treq', 'twisted',
-                      'python-dateutil',
-                      'jinja2']
-)
+from setuptools import setup
+setup()
