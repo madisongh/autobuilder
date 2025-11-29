e1ff5dc44350caa8f74a0a05bb22bfe29327ed75
Matt Madison <matt@madison.systems>
2025-11-29 05:46:23 -0800

    Remove fixup for Twisted ESTMP sender class
    buildbot 4.2+ requires Twisted 22.1.0 or later, so
    this fixup is no longer needed.
    
    Signed-off-by: Matt Madison <matt@madison.systems>

--- autobuilder/__init__.py
+++ autobuilder/__init__.py
@@ -1,4 +1,3 @@
-from .hackery import myESMTPSender
 from .abconfig import AutobuilderConfig, Repo
 from .workers.config import EC2Params, AutobuilderWorker, AutobuilderEC2Worker
 from .distros.config import Distro, TargetImageSet, TargetImage, SdkImage, Buildtype
@@ -7,9 +6,3 @@ from .factory.distro import DistroImage
 from .github.handler import AutobuilderGithubEventHandler
 from .aws_secretsprovider.aws_secrets import AWSSecretsManagerProvider
 from .message_utils import AutobuilderMessageFormatter, AutobuilderMessageTemplate
-
-import twisted
-from twisted.mail.smtp import ESMTPSenderFactory
-
-if twisted.version.major == 20 and twisted.version.minor <= 3:
-    ESMTPSenderFactory.protocol = myESMTPSender
